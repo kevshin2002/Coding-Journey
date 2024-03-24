c97913757a9a6d2aad0e06af5eba9ee8b6d282cb
Kevin Shin <kevshin2002@gmail.com>
2024-03-23 20:33:30 -0700

    Logger

--- Tools/Logging/Logger.hpp
+++ Tools/Logging/Logger.hpp
@@ -1,6 +1,13 @@
 /*
 	Created By Kevin Shin
 	Logger.hpp
+
+In order to use this logger, you must instantiate it within your block as LOGGER(aPath, aFile, Flags)
+The types are as follows:
+aPath: specifies the folder path and must be a string, const char*, or a fs::path type.
+aFile: specifies the file name and must be either a string or const char*.
+Flags: These must be compatible to ios_base to open any type of streams.file.
+
 */
 
 #ifndef Logger_hpp
@@ -10,23 +17,40 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <unordered_map>
 
-namespace fs = std::filesystem;
+namespace Debug {
+	namespace fs = std::filesystem;
 
-namespace tool {
+	template<typename Folder>
+	using isValidFolder = std::enable_if_t<
+		std::is_same<std::decay_t<Folder>, std::string>::value ||
+		std::is_same<std::decay_t<Folder>, fs::path>::value || 
+		std::is_same<std::decay_t<Folder>, const char*>::value,
+		bool
+	>;
+
+	template<typename FileName>
+	using isValidFile = std::enable_if_t<
+		std::is_same<std::decay_t<FileName>, std::string>::value ||
+		std::is_same<std::decay_t<FileName>, const char*>::value,
+		bool
+	>;
+
 	class Logger {
 	public:
-		template <typename Folder, typename FileName>
-		static Logger& instance(Folder&& aFolder, FileName&& aName) {
-			static_assert(std::is_same<std::decay_t<Folder>, std::string>::value || std::is_same<std::decay_t<Folder>, const char*>::value, "Path must be a string or a char*");
+		Logger(const Logger&) = delete;
+		Logger& operator=(const Logger&) = delete;
 
-			static std::string thePath = std::string(std::forward<Folder>(aFolder)) + "/" +  std::forward<FileName>(aName);
-			static std::fstream theStream(thePath, std::fstream::out);
-
+		template<typename Folder, typename FileName, typename FileFlags = std::ios_base::openmode,
+			isValidFolder<Folder> = true,
+			isValidFile<FileName> = true>
+		static Logger& instance(Folder&& aFolder, FileName&& aName, FileFlags aFlags) {
+			std::string thePath = std::string(std::forward<Folder>(aFolder)) + "/" + std::forward<FileName>(aName);
+			static std::fstream theStream(thePath, aFlags);
 			static Logger theInstance(theStream);
 			return theInstance;
 		}
-
 		std::ostream& info(const std::string_view& aMessage) {
 			output << "INFO: " << aMessage << "\n";
 			return output;
@@ -59,24 +83,15 @@ namespace tool {
 			output << "WARN: " << aMsg1 << " " << aMsg2 << "\n";
 			return output;
 		}
-
-	protected:
-		Logger(std::ostream& anOut) : output(anOut) {}
-		Logger(const Logger& aLogger) = delete;
-		~Logger() {}
-
 	private:
 		std::ostream& output;
+		Logger(std::ostream& anOut) : output(anOut) {}
 	};
 }
 
-// FIX THIS
-// DOESN"T ALLOW USER TO SET PATH
-// CONSIDER ADDING MULTIPLE LOGGERS SUPPORT
-
 #define __LOGGING__
 #ifdef __LOGGING__
-	static tool::Logger& theLogger = tool::Logger::instance("C:\\Users\\herb0\\OneDrive\\Desktop\\Github\\Coding Journey\\Tools\\Logging\\Files", "test1.txt");
+    #define LOGGER(aPath, aFile, aFlags) static Debug::Logger& theLogger = Debug::Logger::instance(aPath, aFile, aFlags);
 	#define LOG_INFO(aMsg) theLogger.info(aMsg);
 	#define LOG_WARN(aMsg) theLogger.warn(aMsg);
 #else

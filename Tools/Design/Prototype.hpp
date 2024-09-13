28214cba2e6914e3fde9a08923dd8eb049680e5b
Kevin Shin <kevshin2002@gmail.com>
2024-09-12 23:56:08 -0700

    Contains Duplicates

--- Tools/Design/Prototype.hpp
+++ Tools/Design/Prototype.hpp
@@ -6,13 +6,33 @@
 #ifndef Prototype_hpp
 #define Prototype_hpp
 
+#include <unordered_map>
+#include <memory>
+
 namespace Creational {
+	template <typename derivedClass>
+	using PrototypeState = std::shared_ptr<derivedClass>
 	class Prototype {
 	public:
+		Prototype(){}
+		~Prototype(){}
+
+		PrototypeState 
 	protected:
+
+
 	private:
+
+		Prototype(Prototype& aPrototype) = delete;
+		std::unordered_map<uint32_t, ProtoTypeState> states;
 	};
 
 }
 
 #endif // Prototype.hpp
+
+/*
+We create a class and want a history of their old versions.
+
+
+*/
--- Tools/Logging/main.cpp
+++ Tools/Logging/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "Logger.hpp"
 
+
 int main() {
-	std::cout << "Test";
+	/*std::string aPath = "C:\\Users\\herb0\\OneDrive\\Desktop\\Github\\Coding Journey\\Tools\\Logging\\Files";
+	std::string aFile = "test3.txt";
+	LOGGER(aPath, aFile, std::fstream::out);
 	LOG_INFO("TEST");
 	LOG_WARN("TEST2");
+	*/
 }
